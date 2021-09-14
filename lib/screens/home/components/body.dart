fc129babf1b2d8ac92a17f917735c704a0ac668c
Jhon A. Briones <joanbric@gmail.com>
2021-09-14 11:22:47 -0600

    Categories done

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'categories.dart';
+
+class Body extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+              "Women", 
+              style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight: FontWeight.bold),
+              ),
+        ),
+        
+        Categories(),
+        
+        ],
+    );
+  }
+  
+}
+
